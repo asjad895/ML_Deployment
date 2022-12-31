f8f7a23ada6a82d3ff3597c4fb132c4646ce5f2c
Arslan1k <105630727+Arslan1k@users.noreply.github.com>
2022-12-31 15:08:34 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,7 +197,7 @@ if st.button(label="Check Status"):
     st.write("Your loan is Approved!")
 
 st.header("")
-st.text("__________________________________________")
+st.caption("__________________________________________________________________")
 st.subheader('App Deployment Team')
 st.markdown("The completion of the project [**Credit Risk Analysis**](https://github.com/Arslan1k/ML_Deployment) is dedicated to ([**TechnoColabs**](https://technocolabs.com/)). Special thanks to CEO ([**Yasin Shah**](https://www.linkedin.com/in/yasinshah9598/)) and Mentor ([**Mitesh Verma**](https://www.linkedin.com/in/mitesh-verma-049b12b3/)) for leading us with precious guidance and experience throught the internship period.")
 
