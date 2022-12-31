2b79b87a1c8ae0afcc20e6866fe25065eaa346cb
Arslan1k <105630727+Arslan1k@users.noreply.github.com>
2022-12-31 14:52:47 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -197,16 +197,13 @@ if st.button(label="Check Status"):
     st.write("Your loan is Approved!")
 
 st.subheader('App Deployment Team')
-st.markdown(The completion of this project is dedicated to ([**TechnoColabs**](https://technocolabs.com/)). 
-            Special thanks to CEO ([**Yasin Shah**](https://www.linkedin.com/in/yasinshah9598/)) 
-            and Mentor ([**Mitesh Verma**](https://www.linkedin.com/in/mitesh-verma-049b12b3/)) 
-            for leading us with precious guidance and experience throught the internship period)
+st.markdown("The completion of this project is dedicated to ([**TechnoColabs**](https://technocolabs.com/)). Special thanks to CEO ([**Yasin Shah**](https://www.linkedin.com/in/yasinshah9598/)) and Mentor ([**Mitesh Verma**](https://www.linkedin.com/in/mitesh-verma-049b12b3/)) for leading us with precious guidance and experience throught the internship period.")
 
-### Project Team
-- [Arslan Mehmood](https://www.linkedin.com/in/arslan-mehmood-1k/) (Team Leader)
-- [Kalinga Moharana](https://www.linkedin.com/in/kalinga-moharana-174881205/)
-- [Aditya Kurhade](https://www.linkedin.com/in/kurhadeaditya//)
-- [Md Asjadullah](https://www.linkedin.com/in/md-asjad-314501211)
-- [Robair Garas](https://www.linkedin.com/in/robairgaras) 
+st.markdown("Project Team")
+st.markdown("- [Arslan Mehmood](https://www.linkedin.com/in/arslan-mehmood-1k/) (Team Leader)")
+st.markdown("- [Kalinga Moharana](https://www.linkedin.com/in/kalinga-moharana-174881205/)")
+st.markdown("- [Aditya Kurhade](https://www.linkedin.com/in/kurhadeaditya//)")
+st.markdown("- [Md Asjadullah](https://www.linkedin.com/in/md-asjad-314501211)")
+st.markdown("- [Robair Garas](https://www.linkedin.com/in/robairgaras) ")
 
 
