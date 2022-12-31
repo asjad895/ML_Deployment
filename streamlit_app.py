1ac9cf46790c5024cfb4b6346ae72ca98d75f095
Arslan1k <105630727+Arslan1k@users.noreply.github.com>
2022-12-31 23:13:27 +0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -103,7 +103,7 @@ def Regressor():
 
 st.title('Bandora Loan Approval Dashboard')
 
-tab1, tab2 = st.tabs(["Credit Risk Analysis", "Acknowledgment"])
+tab1, tab2 = st.tabs(["**Credit Risk Analysis**", "**Acknowledgment**"])
 
 with tab1:
   st.markdown("The credit risk is analyzed to measure the _possibility of loss as result of borrower failing to repay a loan or meeting the loan obligations_. The more the credit risk the more it has a negative impact on performance of bank. The credit risk analysis is important because it allows the bank to plan strategies to avoid a negative outcome ahead in future.")
